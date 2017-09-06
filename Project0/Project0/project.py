1a1b83a773663da80865c893b2cbd2f2acd10d39
Sean Joseph <joseph62@egr.msu.edu>
2017-09-06 09:58:40 -0400

    Project 0 complete

---
+++ Project0/Project0/project.py
@@ -0,0 +1,43 @@
+import ply.lex as lex
+from sys import stdin
+
+tokens = ('NEWLINE', 'CHARACTER', 'DIGIT')
+
+
+def t_NEWLINE(t):
+    r'\n'
+    return t
+
+def t_DIGIT(t):
+    r'[0-9]'
+    return t
+
+def t_CHARACTER(t):
+    r'.'
+    return t
+
+def t_error(t):
+    pass
+
+
+data = '''3 + 4 * 10
+129 + 32
+'''
+
+
+# Build the lexer
+lexer = lex.lex()
+
+# Give the lexer some input
+input_str = stdin.read()
+lexer.input(input_str)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break  # No more input
+    if tok.type == 'NEWLINE':
+        print(tok.type)
+    else:
+        print(tok.value, tok.type)
