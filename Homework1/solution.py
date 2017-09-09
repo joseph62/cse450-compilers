75f3d8eba89fd0b945787adab5de37d34f1eddc9
Sean Joseph <joseph62@egr.msu.edu>
2017-09-09 16:16:29 -0400

    Homework1 done

---
+++ Homework1/solution.py
@@ -0,0 +1,50 @@
+#Name: Sean Joseph
+
+import re
+import ply.lex as lex
+from sys import stdin
+
+# Question 1
+def chemical_check(input_string):
+    expression = re.compile(
+            r"^[a-zA-Z]+[a-zA-Z0-9]*$"
+            )
+
+    return expression.match(input_string) is not None
+
+
+# Question 2
+
+tokens = ('NOUN','VERB','PUNCTUATION','UNKOWN')
+
+def t_PUNCTUATION(t):
+    r'!'
+    return t
+
+def t_NOUN(t):
+    r'(cat|ferret|dog|human|MSU|Daenerys|Ghost|Josh|you)'
+    return t
+
+def t_VERB(t):
+    r'(is|chases|loves)'
+    return t
+
+def t_UNKOWN(t):
+    r'[a-zA-Z0-9]+'
+    return t
+
+def t_WHITESPACE(t):
+    r'[ \n\t]'
+    return None
+
+lexer = lex.lex()
+input_str = stdin.read()
+lexer.input(input_str)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break # all done
+    dir(tok)
+    print(tok.value,tok.type)
+            
