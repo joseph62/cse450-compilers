bda83aa7e573ec8e9378e5de14d218f609c16795
Sean Joseph <joseph62@egr.msu.edu>
2017-09-25 23:34:36 -0400

    Project 2 finished

--- Project2/Project2/project.py
+++ Project2/Project2/project.py
@@ -3,6 +3,7 @@
 import sys
 import ply.lex as lex
 import ply.yacc as yacc
+import re
 
 special_words = {
         'val' : 'TYPE',
@@ -19,14 +20,20 @@ tokens = ("TYPE","COMMAND_PRINT","COMMAND_RANDOM",
         "COMP_LTE","COMP_GTR","COMP_GTE","BOOL_AND","BOOL_OR",
         "WHITESPACE","COMMENT","UNKNOWN")
 
-literals = ["+","-","*","/","(",")","=",",","{","}","[","]",".",";"]
+literals = "+-*/()={}[].;!,"
 
-#precedence = (('left', '+'),)
+SYMBOLS = {}
+
+precedence = (('left', '*', '/', '+', '-','COMP_LESS','COMP_GTR',
+    'COMP_LTE','COMP_GTE','COMP_EQU','COMP_NEQU','BOOL_AND','BOOL_OR'),
+
+                ('usage','assignment'),
+                )
 
 # LEXER RULES
 def t_newline(t):
     r'\n'
-    #t.lexer.lineno += 1
+    t.lexer.lineno += 1
     t.type = 'WHITESPACE'
 
 def t_WHITESPACE(t):
@@ -40,6 +47,13 @@ def t_ID(t):
 
 def t_VAL_LITERAL(t):
     r'([0-9]*\.[0-9]+|[0-9]+)'
+    string_with_a_period = "^[0-9]*\.[0-9]*$"
+    regex = re.compile(string_with_a_period)
+    if regex.match(t.value):
+        # VAL_LITERAL is a float
+        t.value = float(t.value)
+    else:
+        t.value = int(t.value)
     return t
 
 def t_CHAR_LITERAL(t):
@@ -102,16 +116,10 @@ def t_BOOL_OR(t):
     r'\|\|'
     return t
 
-def t_UNKNOWN(t):
-    r'.'
-    #print("Unknown token on line {}: {}".format(t.lexer.lineno,t.value))
-    sys.exit(0)
-    return t
-
 # Example implementation from the ply
 # documentation
 def t_error(t):
-    print("Bad character: {}".format(t.value))
+    print("Unknown token on line {}: {}".format(t.lexer.lineno,t.value))
     sys.exit(1)
 # END OF TOKEN DEFINITIONS
 
@@ -121,68 +129,281 @@ def p_program(p):
     """
     program : statements
     """
-    print("Valid Program")
+    global SYMBOLS
+    SYMBOLS = {}
+    print("I'm a program!")
 
-def p_empty_statements(p):
+def p_zero_statements(p):
     """
-    statements : 
+    statements :
     """
-    print("Beginning...")
+    print("Started parsing!")
 
 def p_statements(p):
     """
-    statements : statements statement
+    statements : statements statement 
     """
+    pass
+
+def p_declarative_statements(p):
+    """
+    statement : declaration ';'
+    """
+    pass
+
+def p_var_usage_assign_declare_expressions(p):
+    """
+    expression : assignment
+            | usage
+    """
+    p[0] = p[1]
+
+def p_command_expression(p):
+    """
+    expression : function
+    """
+    print("Function call found")
+    p[0] = p[1]
+
+def p_print_command(p):
+    """
+    function : COMMAND_PRINT '(' nonempty_list ')'
+    """
+    print("Print command found")
+    p[0] = None
+
+def p_random_command(p):
+    """
+    function : COMMAND_RANDOM '(' expression ')'
+    """
+    print("Random command found")
+    p[0] = 1337
+
+def p_list(p):
+    """
+    list : empty_list
+    | nonempty_list
+    """
+    print("List found")
+    p[0] = p[1]
+
+def p_nonempty_list(p):
+    """
+    nonempty_list : expression ',' list
+    """
+    print("Nonempty list found")
+    if p[3] is not None:
+        p[0] = [p[1]] + p[3]
+    else:
+        p[0] = [p[1]]
+
+def p_single_element_list(p):
+    """
+    nonempty_list : expression
+    """
+    p[0] = [p[1]]
+
+def p_empty_list(p):
+    """
+    empty_list : 
+    """
+    print("End of list")
+    p[0] = None
+
+def p_var_declaration(p):
+    """
+    declaration : TYPE ID
+    """
+    global SYMBOLS
+    print("Variable Declaration")
+    if p[2] in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[2]] = 0
     p[0] = p[2]
-    print("statements that make for statements")
+
+def p_var_assign(p):
+    """
+    assignment : ID '=' expression
+    """
+    global SYMBOLS
+    print("variable {} is now {}".format(p[1],p[3]))
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[1]] = p[3]
+    p[0] = SYMBOLS[p[1]]
+
+
+def p_var_declare_assign_combo(p):
+    """
+    declaration : TYPE ID '=' expression
+    """
+    global SYMBOLS
+    if p[2] in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[2]] = p[4]
+    print("new variable {} declared with value {}".format(p[2],p[4]))
+    p[0] = SYMBOLS[p[2]]
+
+def p_var_add_assign_operation(p):
+    """
+    assignment : ID ASSIGN_ADD expression
+    """
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[1]] += p[3]
+    p[0] = SYMBOLS[p[1]]
+
+def p_var_subtract_assign_operation(p):
+    """
+    assignment : ID ASSIGN_SUB expression
+    """
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[1]] -= p[3]
+    p[0] = SYMBOLS[p[1]]
+
+def p_var_mulitply_assign_operation(p):
+    """
+    assignment : ID ASSIGN_MULT expression
+    """
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[1]] *= p[3]
+    p[0] = SYMBOLS[p[1]]
+
+
+def p_var_divide_assign_operation(p):
+    """
+    assignment : ID ASSIGN_DIV expression
+    """
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    SYMBOLS[p[1]] /= p[3]
+    p[0] = SYMBOLS[p[1]]
 
 def p_statement(p):
     """
     statement : expression ';'
     """
-    print("Single statement")
-    p[0] = p[1]
+    print("My statement's result: {}".format(p[1]))
 
-def p_empty_expression(p):
+def p_var_dereference_expression(p):
     """
-    expression : 
+    usage : ID
     """
-    print("Empty expression found!")
-    p[0] = None
+    global SYMBOLS
+    if p[1] not in SYMBOLS:
+        raise NameError(p)
+    print("Variable Dereferenced! {} is {}".format(p[1],SYMBOLS[p[1]]))
+    p[0] = SYMBOLS[p[1]]
 
-def p_expression_literal(p):
+def p_negation_expression(p):
+    """
+    expression : negate expression
+    """
+    p[0] = -p[2]
+
+def p_left_expression(p):
+    """
+    negate : '-'
+    """
+    pass
+
+def p_val_literal_expression(p):
     """
     expression : VAL_LITERAL
     """
-    print("Literal found: {}".format(p[1]))
+    print("val literal found!")
     p[0] = p[1]
 
-def p_maths_expression_add(p):
+def p_parens_expression(p):
+    """
+    expression : '(' expression ')'
+    """
+    p[0] = p[2]
+
+def p_arithmetic_expression_add(p):
     """
     expression : expression '+' expression
     """
     p[0] = p[1] + p[3]
 
-def p_maths_expression_subtract(p):
+def p_arithmetic_expression_subtraction(p):
     """
     expression : expression '-' expression
     """
     p[0] = p[1] - p[3]
 
 
-def p_maths_expression_multiply(p):
+def p_arithmetic_expression_multiply(p):
     """
     expression : expression '*' expression
     """
     p[0] = p[1] * p[3]
 
-def p_maths_expression_divide(p):
+
+def p_arithmetic_expression_division(p):
     """
     expression : expression '/' expression
     """
     p[0] = p[1] / p[3]
 
+def p_boolean_expression_equality(p):
+    """
+    expression : expression COMP_EQU expression
+    """
+    p[0] = p[1] == p[3]
+
+def p_boolean_expression_inequality(p):
+    """
+    expression : expression COMP_NEQU expression
+    """
+    p[0] = p[1] != p[3]
+
+def p_boolean_expression_negation(p):
+    """
+    expression : '!' expression
+    """
+    p[0] = not p[2]
+
+def p_boolean_expression_less_than(p):
+    """
+    expression : expression COMP_LESS expression
+    """
+    p[0] = p[1] < p[3]
+
+def p_boolean_expression_greater_than(p):
+    """
+    expression : expression COMP_GTR expression
+    """
+    p[0] = p[1] > p[3]
+
+def p_boolean_expression_less_than_equal(p):
+    """
+    expression : expression COMP_LTE expression
+    """
+    p[0] = p[1] <= p[3]
+
+def p_boolean_expression_greater_than_equal(p):
+    """
+    expression : expression COMP_GTE expression
+    """
+    p[0] = p[1] >= p[3]
+
+def p_boolean_expression_and(p):
+    """
+    expression : expression BOOL_AND expression
+    """
+    p[0] = p[1] and p[3]
+
+def p_boolean_expression_or(p):
+    """
+    expression : expression BOOL_OR expression
+    """
+    p[0] = p[1] or p[3]
+
 def p_error(p):
+    print("Syntax Error!")
     raise SyntaxError(p)
 
 # END OF YACC RULES
@@ -195,11 +416,17 @@ def parse_string(input_):
 
 if __name__ == "__main__":
     source = sys.stdin.read()
-    source = """
-    ;
-    ;
-    ;;
-    4;5;5;
-    """
     parse_string(source)
+
+    '''
+    lexer = lex.lex()
+    lexer.input(source)
+    while True:
+        token = lexer.token()
+        if not token:
+            break
+        print("{}: {}".format(token.type,token.value))
+
+    print("Line Count: {}".format(lexer.lineno - 1))
+    '''
     print("Parse Successful!")
