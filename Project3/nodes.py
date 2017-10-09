ba6cc0c76c9ef6aa4dc31cc759ef8df6fa75bb6f
Sean Joseph <joseph62@egr.msu.edu>
2017-10-09 10:42:32 -0400

    Added a few more nodes. Started looking at lecture notes.

--- Project3/nodes.py
+++ Project3/nodes.py
@@ -1,13 +1,19 @@
 # Name: Sean Joseph
 # This is where all of the abstract syntax tree node classes will be defined.
 
+from tracker import Tracker
+
+# Init the singleton tracker
+tracker = Tracker()
+
 class Node:
     """
     AST Node base class
     """
-    def __init__(self,children=None,data=None):
+    def __init__(self,name='Node',children=None,data=None):
         self.children = children
         self.data = data
+        self.name = name
 
     def generate_bad_code(output):
         raise NotImplementedError()
@@ -18,11 +24,26 @@ class ValLiteralNode(Node):
     for a Val Literal Usage
     """
     def __init__(self,data):
-        super().__init__(data=data)
+        super().__init__(name='ValLiteralNode',data=data)
 
-    #TODO: The walls are closing in! Look at the in class examples
+    #TODO: The walls are closing in! Look at the inclass examples
     # to divine what needs doing, dunce.
-    def generate_bad_code(output,tracker):
-        output += "\n#ValLiteral in use!\nval_copy {} {}".format(
-                self.data.get_value(),self.data.get_name()
-                )
+    def generate_bad_code(output):
+        tracker = Tracker()
+        temp_var = "s{}".format(tracker.get_var_num())
+        output.append("val_copy {} {}".format(self.data, temp_var))
+
+class BlockNode(Node):
+    """
+    A block node is simply a node with children.
+    call the generate code function on the children.
+    """
+    def __init__(self,children):
+        super().__init__(name='BlockNode',children=children)
+
+    def generate_bad_code(output):
+        output.append("#{} beginning".format(self.name))
+        for child in self.children:
+            child.generate_bad_code(output)
+        output.append("#{} ending".format(self.name))
+
